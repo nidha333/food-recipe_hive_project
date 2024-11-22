f9b289ff43b9b02471ef44bafc7fd495a4a96c58
nidha333 <thesninidha@gmail.com>
2024-11-21 23:22:46 -0800

    details.dart

--- lib/details.dart
+++ lib/details.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:hive_project_app/database/function.dart';
 
 class Details extends StatelessWidget {
   String? name;
